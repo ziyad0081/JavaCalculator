a6713093eb3debc3c46c0ce2ec413777e52e0341
Chaalel O. Ziyad <omar.ziyad.chaalel@ensia.edu.dz>
2024-10-15 18:04:27 +0100

    Added some checks and help manual display

--- ApplicationLauncher.java
+++ ApplicationLauncher.java
@@ -1,18 +1,19 @@
-import Computer.ComputerWrapper;
+    import Computer.ComputerWrapper;
 
-public class ApplicationLauncher {
-    static ComputerWrapper wrapper ;
+    public class ApplicationLauncher {
+        static ComputerWrapper wrapper ;
 
-    public static void main(String[] args) {
-        
-
-        try {
-            wrapper = new ComputerWrapper(args);
+        public static void main(String[] args) {
             
-        } catch (Exception e) {
-            System.err.println(e.getMessage());
-            return ;
+
+            try {
+                wrapper = new ComputerWrapper(args);
+                wrapper.PrintComputation();    
+            } catch (Exception e) {
+                System.err.println(e.getMessage());
+                System.err.println("Run with arg -i for help manual");
+                return ;
+            }
+            
         }
-        wrapper.PrintComputation();
     }
-}
--- Computer/ComputerWrapper.java
+++ Computer/ComputerWrapper.java
@@ -10,12 +10,36 @@ public class ComputerWrapper {
     Computer computerObj;
     private Double op1;
     private Double op2; 
+    private Boolean help_flag = false;
     final List<String> operations = Arrays.asList("+","/","x","-");
     public ComputerWrapper(String args[]){
+        if(args[0].equals("-i")){
+            help_flag = true;
+            System.out.println(
+                "Usage: java -jar SimpleCalculator.jar [operator] [num1] [num2]\n" + //
+                                        "\n" + //
+                                        "Where:\n" + //
+                                        "  [operator]  The arithmetic operation to perform. It can be one of:\n" + //
+                                        "              +  for addition\n" + //
+                                        "              -  for subtraction\n" + //
+                                        "              x  for multiplication\n" + //
+                                        "\n" + //
+                                        "  [num1]      The first number (integer or decimal).\n" + //
+                                        "  [num2]      The second number (integer or decimal).\n" + //
+                                        "\n" + //
+                                        "Examples:\n" + //
+                                        "  java -jar SimpleCalculator.jar + 5 3    -> Output: 8\n" + //
+                                        "  java -jar SimpleCalculator.jar - 7 2    -> Output: 5\n" + //
+                                        "  java -jar SimpleCalculator.jar * 4 6    -> Output: 24\n" + //
+                                        ""
+            );
+            return;
+        }
         try {
             op1 = Double.parseDouble(args[1]);
             op2 = Double.parseDouble(args[2]);
         } catch (Exception e) {
+
             throw new NumberFormatException("Illegal arguments (Maybe one or both args arent numbers). Exiting.");
         }
         if(!operations.contains(args[0])){
@@ -27,7 +51,9 @@ public class ComputerWrapper {
         if(args.length < 3){
             throw new IllegalArgumentException("Not enough args");
         }
-        
+        if(args.length > 3){
+            throw new IllegalArgumentException("Too Much Args");
+        }
 
         switch (args[0]) {
             case "+":
@@ -52,6 +78,9 @@ public class ComputerWrapper {
     }
 
     public void PrintComputation(){
+        if(help_flag){
+            return;
+        }
         System.out.println("Result is " + compute());
     }
 
