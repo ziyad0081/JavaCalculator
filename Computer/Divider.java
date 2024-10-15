be71165fbbbbfd888a2192294ad23ab295f235db
hadil <hadil.medaouar@ensia.edu.dz>
2024-10-15 13:01:05 +0100

    hadil

---
+++ Computer/Divider.java
@@ -0,0 +1,11 @@
+public class Divider extends Computer {
+    @Override
+    public double compute(double A, double B) {
+        if (B == 0) {
+            throw new ArithmeticException("Cannot divide by zero.");
+        }
+        return A / B;
+    }
+}
+
+
